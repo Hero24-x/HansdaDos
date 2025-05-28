7b82d57373d5912b37830642aee9986f14a6a0b6
Hero24-00 <196810131+Hero24-x@users.noreply.github.com>
2025-05-28 20:29:35 +0530

    Create banner.py

---
+++ banner.py
@@ -0,0 +1,11 @@
+def print_banner():
+    print(r"""
+ _   _                  _           ____   ___  ____  
+| | | | ___  ___  ___  | | ___     |  _ \ / _ \|  _ \ 
+| |_| |/ _ \/ __|/ _ \ | |/ _ \____| | | | | | | | | |
+|  _  | (_) \__ \  __/ | | (_) |___| |_| | |_| | |_| |
+|_| |_|\___/|___/\___| |_|\___/    |____/ \___/|____/ 
+
+        🔥 HansdaDOS by Shibnath Hansda (HansdaTechs) 🔥
+        ⚔️ Educational DDoS Simulation Tool - For Ethical Hackers ⚔️
+    """)
